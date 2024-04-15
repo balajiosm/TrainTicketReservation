c4eb292c617c788b9681c8113f4fe47e2a3a7bf4
balajiosm <balajiosm@gmail.com>
2024-04-15 13:06:25 +0530

    adding tests one by one

--- TrainTicketReservation/src/test/java/com/ticket/reserve/UserControllerTests.java
+++ TrainTicketReservation/src/test/java/com/ticket/reserve/UserControllerTests.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.ticket.reserve.entity.User;
+import com.ticket.reserve.exception.ErrorResponse;
 
 public class UserControllerTests extends AbstractTest {
 
@@ -26,10 +27,21 @@ public class UserControllerTests extends AbstractTest {
 				.content(inputJson)).andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(200, status);//verifying good request
 		String content = mvcResult.getResponse().getContentAsString();
-		//assertEquals(content, "User is created successfully");
 		assertEquals(content, inputJson);
+	}
+	
+	@Test
+	public void deleteUserThrowsExceptionIfNotPresent() throws Exception {
+		String uri = "/api/v1/deleteUser/1";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+		ErrorResponse resp = new ErrorResponse(400, "User Purchase details not Present hence cannot delete !!!!");
+		String respJson = super.mapToJson(resp);
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(400, status);//verifying bad request
+		String content = mvcResult.getResponse().getContentAsString();
+		assertEquals(content, respJson);
 	}
 
 }
